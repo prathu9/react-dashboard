61dfbfc14fa5823d345460f07dbdecb0cc42ff86
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2024-12-26 19:18:07 +0530

    updated contest section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import { ReactComponent as TONIcon } from "../../assets/icons/TON.svg";
+import { ReactComponent as TONIXIcon } from "../../assets/icons/TONIX.svg";
+import WalletItem from "../../components/WalletItem/WalletItem";
+import "./Home.css";
+
+const walletBalance = [
+  { icon: <TONIcon />, amount: 178.8245, unit: "TON" },
+  { icon: <TONIXIcon />, amount: 100, unit: "TONIX" },
+];
+
 const Home = () => {
-    return(
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
-}   
+  const [openWithdrawal, setOpenWithdrawal] = useState(false);
+
+  const toggleWithdrawal = () => {
+    setOpenWithdrawal(!openWithdrawal);
+  };
+
+  return (
+    <div className="home-section">
+      <div className="wallet-balance">
+        {walletBalance.map(({ icon, amount, unit }) => (
+          <WalletItem
+            handleClick={toggleWithdrawal}
+            icon={icon}
+            amount={amount}
+            unit={unit}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
--- src/pages/Missions/components/Contest/Contest.js
+++ src/pages/Missions/components/Contest/Contest.js
@@ -1,9 +1,56 @@
+import { ReactComponent as InstagramIcon } from "../../../../assets/icons/instagram.svg";
+import { ReactComponent as YoutubeIcon } from "../../../../assets/icons/youtube.svg";
+import { ReactComponent as TikTokIcon } from "../../../../assets/icons/tiktok.svg";
+import { ReactComponent as TONIXIcon } from "../../../../assets/icons/TONIX.svg";
+import {ReactComponent as AddIcon} from "../../../../assets/icons/add.svg";
+import CustomButton from "../../../../components/CustomButton/CustomButton";
+import "./Contest.css";
+
 const Contest = () => {
-    return(
-        <div>
-            Contest
-        </div>
-    )
-}
+  return (
+    <div className="contest-section">
+      <h2 className="title">Tell others about TONIX App</h2>
+      <div className="contest-social-icons">
+        <InstagramIcon />
+        <YoutubeIcon />
+        <TikTokIcon />
+      </div>
+      <div className="separator" />
+      <div className="reward-details">
+        <span>And get up to</span>
+        <span className="reward-prize">
+          <TONIXIcon />
+          <span>10 000 000</span>
+        </span>
+        <span>TONIX for each video</span>
+      </div>
+      <div className="separator" />
+      <CustomButton className="add-content-btn" leftIcon={<AddIcon/>}>
+        Add Content and Earn
+      </CustomButton>
+      <div className="contest-rules">
+        <h3 className="rules-title">Rules</h3>
+        <ul>
+          <li className="rule">
+            <b>Create Content:</b> Make a fan video about TONIX App for YouTube Shorts,
+            Instagram Reels, or TikTok.
+          </li>
+          <li className="rule">
+            <b>Include your ID or Invite Link:</b> Attach your ID or invite link in the
+            video description.
+            <p className="">
+            Get Your Invite Link on Friends
+            section
+            </p>
+          </li>
+          <li className="rule">
+            Send the Link: Once your video reaches 100+ views, send us the link.
+          </li>
+        </ul>
+      </div>
+      <div className="spacer"></div>
+    </div>
+  );
+};
 
 export default Contest;
