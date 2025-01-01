0466adbc4ebbfe5ab8da290a401851da8da5a803
prathamesh sawant <30414105+prathu9@users.noreply.github.com>
2025-01-01 20:36:21 +0530

    updated friends list

--- src/components/FriendsList/FriendsList.js
+++ src/components/FriendsList/FriendsList.js
@@ -1,7 +1,29 @@
+import {ReactComponent as MagnifyGlassHIcon} from "../../assets/icons/magnify-glass-h.svg"; 
+import "./FriendsList.css";
+
 const FriendsList = () => {
     return(
-        <div>
-            Friend list
+        <div className="friend-list">
+            <div className="level-wrapper">
+                <div className="level">
+                    <span>Level 1</span>
+                    <span>0</span>
+                </div>
+                <div className="level">
+                    <span>Level 2</span>
+                    <span>0</span>
+                </div>
+                <div className="level">
+                    <span>Level 3</span>
+                    <span>0</span>
+                </div>
+            </div>
+            <div>
+                <div className="no-friends">
+                    <MagnifyGlassHIcon/>
+                    <span>You have no friends yet...</span>
+                </div>
+            </div>
         </div>
     )
 }
